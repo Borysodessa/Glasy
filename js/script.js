e8836e1ade9592dc8956c6abb2ab773cf62f3c17
Borysodessa / <chayka2000@mail.ru>
2020-11-21 19:58:40 +0300

    модальное окно

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+const feedbackButton = document.querySelector(".feedback__button");
+const modalForm = document.querySelector(".modal__form");
+const closeModal = modalForm.querySelector(".modal-close");
+const sliderControl = document.querySelector(".slider-control__list:first-child");
+console.log(sliderControl);
+
+feedbackButton.addEventListener("click",function(evt){
+  evt.preventDefault(evt);
+  modalForm.classList.remove('visually-hidden');
+});
+
+closeModal.addEventListener("click",function(evt){
+  evt.preventDefault();
+  modalForm.classList.add('visually-hidden');
+});
+
+
